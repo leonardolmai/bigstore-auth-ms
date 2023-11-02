a0723a4a33778712dac3088d0fabd405a0cc63e9
Leonardo Lima <se.leonardolima@gmail.com>
2023-11-02 13:34:42 -0300

    Include user and token routers

--- src/main/server/server.py
+++ src/main/server/server.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
 
+from src.presentation.routers import token_routers, user_routers
+
 app = FastAPI()
 
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+app.include_router(user_routers.router)
+app.include_router(token_routers.router)
