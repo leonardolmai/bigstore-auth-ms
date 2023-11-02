9d77b7d92fb79eca726caf501f2dbf8d3637c7ac
Leonardo Lima <se.leonardolima@gmail.com>
2023-11-02 20:00:22 -0300

    Add get user composer

---
+++ src/presentation/composers/get_user_composer.py
@@ -0,0 +1,14 @@
+from src.data.use_cases.get_user import GetUserUseCase
+from src.infrastructure.database.repositories.user_repository import (
+    UserRepository,
+)
+from src.presentation.controllers.get_user_controller import GetUserController
+from src.presentation.schemas.user import UserOut
+
+
+def get_user_composer(session, email) -> UserOut | None:
+    repository = UserRepository(session)
+    use_case = GetUserUseCase(repository)
+    controller = GetUserController(use_case)
+    user = controller.handle(email)
+    return user
