9ba2d23cbc37bb33420ab1073a9ce47fdeb628cf
Leonardo Lima <se.leonardolima@gmail.com>
2023-11-02 19:44:48 -0300

    Add create user composer

---
+++ src/presentation/composers/create_user_composer.py
@@ -0,0 +1,16 @@
+from src.data.use_cases.create_user import CreateUserUseCase
+from src.infrastructure.database.repositories.user_repository import (
+    UserRepository,
+)
+from src.presentation.controllers.create_user_controller import (
+    CreateUserController,
+)
+from src.presentation.schemas.user import UserOut
+
+
+def create_user_composer(session, user) -> UserOut | None:
+    repository = UserRepository(session)
+    use_case = CreateUserUseCase(repository)
+    controller = CreateUserController(use_case)
+    user = controller.handle(user)
+    return user
