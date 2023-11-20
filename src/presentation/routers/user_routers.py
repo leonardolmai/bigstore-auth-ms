6bfeb2da3b0bed72b8f84f80789f6248a797a594
Leonardo Lima <se.leonardolima@gmail.com>
2023-11-20 09:55:48 -0300

    Add a company cnpj header parameter to routes

--- src/presentation/routers/user_routers.py
+++ src/presentation/routers/user_routers.py
@@ -1,25 +1,18 @@
+# pylint: disable=unused-argument
 from typing import Annotated
 
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, Header, HTTPException, status
 from sqlalchemy.orm import Session
 
 from src.infrastructure.database.settings.db_connection import get_db
 from src.main.settings.config import oauth2_scheme
-from src.presentation.composers.user.create_user_composer import (
-    create_user_composer,
-)
-from src.presentation.composers.user.delete_user_composer import (
-    delete_user_composer,
-)
+from src.presentation.composers.user.create_user_composer import create_user_composer
+from src.presentation.composers.user.delete_user_composer import delete_user_composer
 from src.presentation.composers.user.get_authenticated_user_composer import (
     get_authenticated_user_composer,
 )
-from src.presentation.composers.user.list_users_composer import (
-    list_users_composer,
-)
-from src.presentation.composers.user.update_user_composer import (
-    update_user_composer,
-)
+from src.presentation.composers.user.list_users_composer import list_users_composer
+from src.presentation.composers.user.update_user_composer import update_user_composer
 from src.presentation.schemas.user import UserCreate, UserOut, UserUpdate
 
 router = APIRouter(prefix="/users", tags=["users"])
@@ -28,6 +21,7 @@ router = APIRouter(prefix="/users", tags=["users"])
 @router.get("/", status_code=status.HTTP_200_OK, response_model=list[UserOut])
 def list_users(
     token: Annotated[str, Depends(oauth2_scheme)],
+    x_company_cnpj: Annotated[str, Header()],
     session: Session = Depends(get_db),
 ):
     user: UserOut = get_authenticated_user_composer(session, token)
@@ -40,6 +34,7 @@ def list_users(
 @router.get("/me", status_code=status.HTTP_200_OK, response_model=UserOut)
 def get_user(
     token: Annotated[str, Depends(oauth2_scheme)],
+    x_company_cnpj: Annotated[str, Header()],
     session: Session = Depends(get_db),
 ):
     user = get_authenticated_user_composer(session, token)
@@ -49,7 +44,11 @@ def get_user(
 
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=UserOut)
-def create_user(user: UserCreate, session: Session = Depends(get_db)):
+def create_user(
+    user: UserCreate,
+    x_company_cnpj: Annotated[str, Header()],
+    session: Session = Depends(get_db),
+):
     user = create_user_composer(session, user)
     if user:
         return user
@@ -62,6 +61,7 @@ def create_user(user: UserCreate, session: Session = Depends(get_db)):
 def update_user(
     email: str,
     user: UserUpdate,
+    x_company_cnpj: Annotated[str, Header()],
     token: Annotated[str, Depends(oauth2_scheme)],
     session: Session = Depends(get_db),
 ):
@@ -76,6 +76,7 @@ def update_user(
 
 @router.delete("/", status_code=status.HTTP_204_NO_CONTENT)
 def delete_user(
+    x_company_cnpj: Annotated[str, Header()],
     token: Annotated[str, Depends(oauth2_scheme)],
     session: Session = Depends(get_db),
 ):
