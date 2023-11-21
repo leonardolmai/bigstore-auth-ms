dabe22f2fdde2010e90890110e6c667564b70620
Leonardo Lima <se.leonardolima@gmail.com>
2023-11-21 07:56:26 -0300

    Update the repository with the delete method and the list method and update the create method

--- src/data/repositories/user_company_repository.py
+++ src/data/repositories/user_company_repository.py
@@ -1,10 +1,18 @@
 from abc import ABC, abstractmethod
 from typing import Any
 
+from src.domain.entities.company import Company
+from src.domain.entities.user import User
 from src.domain.entities.user_company import UserCompany
 
 
 class UserCompanyRepositoryInterface(ABC):
+    @abstractmethod
+    def list_user_companies(
+        self, user: User, company: Company
+    ) -> list[UserCompany] | None:
+        pass
+
     @abstractmethod
     def get_user_company(self, id: int) -> UserCompany | None:
         pass
@@ -17,4 +25,7 @@ class UserCompanyRepositoryInterface(ABC):
     def update_user_company(
         self, id: int, update_fields: dict[str, Any]
     ) -> UserCompany | None:
+        pass
+
+    def delete_user_company(self, user_id: int, company_id: int) -> bool:
         pass
--- src/infrastructure/database/repositories/user_company_repository.py
+++ src/infrastructure/database/repositories/user_company_repository.py
@@ -3,6 +3,8 @@ from typing import Any
 from sqlalchemy.orm import Session
 
 from src.data.repositories.user_company_repository import UserCompanyRepositoryInterface
+from src.domain.entities.company import Company
+from src.domain.entities.user import User
 from src.domain.entities.user_company import UserCompany
 from src.infrastructure.database.models.user_company import (
     UserCompany as UserCompanyModel,
@@ -12,6 +14,18 @@ from src.infrastructure.database.models.user_company import (
 class UserCompanyRepository(UserCompanyRepositoryInterface):
     def __init__(self, session: Session):
         self.session: Session = session
+
+    def list_user_companies(
+        self, user: User, company: Company
+    ) -> list[UserCompany] | None:
+        try:
+            return (
+                self.session.query(UserCompanyModel)
+                .filter_by(company_id=company.id, is_employee=True)
+                .all()
+            )
+        except:
+            return None
 
     def get_user_company(self, id: int) -> UserCompany | None:
         try:
@@ -31,6 +45,18 @@ class UserCompanyRepository(UserCompanyRepositoryInterface):
         )
 
         if existing_relation:
+            if not existing_relation.is_employee and user_company.is_employee:
+                try:
+                    self.session.query(UserCompanyModel).filter(
+                        UserCompanyModel.id == existing_relation.id
+                    ).update({UserCompanyModel.is_employee: True})
+                    self.session.commit()
+                    user_company_updated = self.get_user_company(existing_relation.id)
+
+                    return user_company_updated
+                except:
+                    return None
+
             return existing_relation
 
         try:
@@ -61,3 +87,21 @@ class UserCompanyRepository(UserCompanyRepositoryInterface):
             return user_company_updated
         except:
             return None
+
+    def delete_user_company(self, user_id: int, company_id: int) -> bool:
+        existing_relation = (
+            self.session.query(UserCompanyModel)
+            .filter_by(user_id=user_id, company_id=company_id)
+            .first()
+        )
+
+        try:
+            if existing_relation:
+                self.session.query(UserCompanyModel).filter(
+                    UserCompanyModel.id == existing_relation.id
+                ).update({UserCompanyModel.is_employee: False})
+                self.session.commit()
+                return True
+        except:
+            return False
+        return False
