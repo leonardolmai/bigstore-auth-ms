0d953856dcd7bd68a13c533aa9c1e1c1c1f84238
Leonardo Lima <se.leonardolima@gmail.com>
2023-11-21 08:18:19 -0300

    Add employees routes

--- src/presentation/routers/company_routers.py
+++ src/presentation/routers/company_routers.py
@@ -19,9 +19,20 @@ from src.presentation.composers.company.list_companies_composer import (
 from src.presentation.composers.company.update_company_composer import (
     update_company_composer,
 )
+from src.presentation.composers.user_company.create_user_company_composer import (
+    create_user_company_composer,
+)
+from src.presentation.composers.user_company.delete_user_company_composer import (
+    delete_user_company_composer,
+)
+from src.presentation.composers.user_company.list_user_companies_composer import (
+    list_user_companies_composer,
+)
+from src.presentation.dependencies.get_current_company import get_current_company
 from src.presentation.dependencies.get_current_user import get_current_user
 from src.presentation.schemas.company import CompanyCreate, CompanyOut, CompanyUpdate
 from src.presentation.schemas.user import UserOut
+from src.presentation.schemas.user_company import UserCompanyOut
 
 router = APIRouter(prefix="/companies", tags=["companies"])
 
@@ -107,4 +118,67 @@ def delete_company(
         return
     raise HTTPException(
         status_code=status.HTTP_404_NOT_FOUND, detail="Company not found."
+    )
+
+
+@router.get(
+    "/employees/",
+    status_code=status.HTTP_200_OK,
+    response_model=list[UserCompanyOut],
+    tags=["employees"],
+)
+async def list_employees(
+    x_company_cnpj: Annotated[str, Header()],
+    current_company: CompanyOut = Depends(get_current_company),
+    current_user: UserOut = Depends(get_current_user),
+    session: Session = Depends(get_db),
+):
+    if current_company:
+        employees = list_user_companies_composer(session, current_user, current_company)
+        return employees
+    raise HTTPException(
+        status_code=status.HTTP_400_BAD_REQUEST, detail="Employees not found."
+    )
+
+
+@router.post(
+    "/employees/",
+    status_code=status.HTTP_201_CREATED,
+    response_model=UserCompanyOut,
+    tags=["employees"],
+)
+def add_employee(
+    email: str,
+    x_company_cnpj: Annotated[str, Header()],
+    current_company: CompanyOut = Depends(get_current_company),
+    current_user: UserOut = Depends(get_current_user),
+    session: Session = Depends(get_db),
+):
+    if current_company:
+        employee = create_user_company_composer(session, current_company, email)
+        if employee:
+            return employee
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND, detail="Employee not found."
+    )
+
+
+@router.delete(
+    "/employees/",
+    status_code=status.HTTP_204_NO_CONTENT,
+    tags=["employees"],
+)
+def remove_employee(
+    email: str,
+    x_company_cnpj: Annotated[str, Header()],
+    current_company: CompanyOut = Depends(get_current_company),
+    current_user: UserOut = Depends(get_current_user),
+    session: Session = Depends(get_db),
+):
+    if current_company:
+        employee = delete_user_company_composer(session, current_company, email)
+        if employee:
+            return
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND, detail="Employee not found."
     )
